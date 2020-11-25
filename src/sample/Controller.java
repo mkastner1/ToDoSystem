4be64d0381e4b7c901b248b90464e1edb6c0391f
mkastner <mkastner@htl-steyr.ac.at>
2020-11-25 09:20:01 +0100

    ToDoSystem

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,30 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class Controller {
+    public void onStatusClicked(ActionEvent actionEvent) {
+
+    }
+
+    public void onPriorityClicked(ActionEvent actionEvent) {
+        Parent root = null;
+        try {
+            Stage stage = new Stage();
+
+            root = FXMLLoader.load(getClass().getResource("priority.fxml"));
+            stage.setTitle("Prioritäten");
+            stage.setScene(new Scene(root, 300, 275));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
---
+++ src/sample/PriorityController.java
@@ -0,0 +1,23 @@
+package sample;
+
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import sample.model.Priority;
+
+public class PriorityController {
+    public TextField nameTextField;
+    public ListView<Priority> priorityListView;
+
+    public void initialize() {
+        priorityListView.setItems(Priority.getList());
+    }
+
+    public void itemSelected(MouseEvent mouseEvent) {
+        Priority p = priorityListView.getSelectionModel().getSelectedItem();
+
+        if (p != null) {
+            nameTextField.setText(p.getName());
+        }
+    }
+}
